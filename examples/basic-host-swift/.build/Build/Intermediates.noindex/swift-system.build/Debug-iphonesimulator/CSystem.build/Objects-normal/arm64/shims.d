0b86de1d23bd5baad8072f466608b5cb84d642ee
Olivier Chafik <ochafik@anthropic.com>
2026-01-08 13:44:26 +0000

    rm

--- examples/basic-host-swift/.build/Build/Intermediates.noindex/swift-system.build/Debug-iphonesimulator/CSystem.build/Objects-normal/arm64/shims.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ochafik/code/ext-apps3/examples/basic-host-swift/.build/SourcePackages/checkouts/swift-system/Sources/CSystem/include/module.modulemap \
-  /Users/ochafik/code/ext-apps3/examples/basic-host-swift/.build/SourcePackages/checkouts/swift-system/Sources/CSystem/shims.c
